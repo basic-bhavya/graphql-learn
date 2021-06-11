b7d7e6e8204d3d49ee7e6354710d901f4073213f
basic-bhavya <bjain.nd@gmail.com>
2021-06-11 11:14:54 +0530

    make CRUD

--- src/index.js
+++ src/index.js
@@ -1,27 +1,53 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-  }
-`
+let links = [
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
+];
 
-// 2
+let idCount = links.length;
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
-  }
-}
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (_, args) => links.find((val) => val.id === args.id),
+  },
+  Mutation: {
+    post: (_, args) => {
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+      links.push(link);
+      return link;
+    },
+    updateLink: (_, args) => {
+      const toUpdate = links.find((link) => {
+        return link.id == args.id;
+      });
+      toUpdate.url = args.url;
+      toUpdate.description = args.description;
+    },
+    deleteLink: (_, args) => {
+      const removed = links.splice(
+        links.find((link) => link.id === args.id),
+        1
+      );
+      return removed[0];
+    },
+  },
+};
 
 // 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf-8"),
   resolvers,
-})
+});
 
-server
-  .listen()
-  .then(({ url }) =>
-    console.log(`Server is running on ${url}`)
-  );
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
